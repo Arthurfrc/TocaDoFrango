bedc8fcd2547f6c85111bc230035bba96683ed52
Arthur Felipe <artur-felipe-14@hotmail.com>
2026-04-09 19:04:11 -0300

    [FIX] Firebase auth remove

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,7 +1,6 @@
 // src/config/firebase.ts
 
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
@@ -16,7 +15,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app)
 export const storage = getStorage(app);
 export const db = getFirestore(app);
 export default app;
